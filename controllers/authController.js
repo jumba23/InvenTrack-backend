d6a733ebe1a2ee8f3ee7b5ca34e2eb2743b483e3
Goran SD <g.cvetic23@gmail.com>
2023-10-26 05:31:47 -0700

    commented out unnec code

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,11 +5,17 @@ const supabaseKey = process.env.SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 exports.signup = async (req, res) => {
-  const { username, password } = req.body;
+  const { email, password, firstName, lastName, cellNumber } = req.body;
 
-  // Implement your signup logic here, probably using Supabase
+  console.log("email", email);
+  console.log("password", password);
+  console.log("firstName", firstName);
+  console.log("lastName", lastName);
+  console.log("cellNumber", cellNumber);
+
+  // Implement your signup logic here, using Supabase
   const { user, error } = await supabase.auth.signUp({
-    email: username,
+    email: email,
     password: password,
   });
 
@@ -17,7 +23,28 @@ exports.signup = async (req, res) => {
     return res.status(400).json({ error: error.message });
   }
 
-  res.status(201).json({ user });
+  if (!user) {
+    return res.status(400).json({ error: "User not created" });
+  }
+
+  // Wait for 5 seconds to give the user time to click on the verification link
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+  console.log("create user", user);
+
+  // Once the user is successfully created, save additional profile data
+  // const { data, error: insertError } = await supabase.from("profiles").insert({
+  //   user_id: user.id,
+  //   first_name: firstName,
+  //   last_name: lastName,
+  //   cell_number: cellNumber,
+  // });
+
+  // if (insertError) {
+  //   return res.status(400).json({ error: insertError.message });
+  // }
+
+  // // Return both the user and their additional data
+  // res.status(201).json({ user, profile: data });
 };
 
 exports.login = async (req, res) => {
@@ -35,6 +62,9 @@ exports.login = async (req, res) => {
     return res.status(400).json({ error: error.message });
   }
 
+  if (!session) {
+    return res.status(400).json({ error: "Session not created" });
+  }
   // Store the token or session info on the server side or return it to the client
   // In this example, we'll return it to the client
   res.status(200).json({ user, token: session.access_token });
