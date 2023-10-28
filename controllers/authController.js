db8c3babe08699e02439d3d2db6af0614ecb5c25
Goran SD <g.cvetic23@gmail.com>
2023-10-28 06:47:58 -0700

    removed unused code

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,4 @@
 const { createClient } = require("@supabase/supabase-js");
-const to = require("to");
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_ANON_KEY;
