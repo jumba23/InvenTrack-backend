d6e9577b49ab23766788b64624ffeb36ce0c1f19
Goran SD <g.cvetic23@gmail.com>
2023-11-17 20:52:46 -0800

    Add JWT validation middleware

---
+++ middleware/validateJWT.js
@@ -0,0 +1,16 @@
+module.exports = function validateJWT(req, res, next) {
+  const token = req.cookies.authToken;
+  if (!token) {
+    return res.status(401).send("Access Denied");
+  }
+
+  try {
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+    next();
+  } catch (err) {
+    res.status(400).send("Invalid Token");
+  }
+
+  next();
+};
