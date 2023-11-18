0c21838466ea77813ec07dcf8daf4e4afd9ff46a
Goran SD <g.cvetic23@gmail.com>
2023-11-18 11:03:17 -0800

    Add JWT validation route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
+const validateJWT = require("../middleware/validateJWT");
 
+// New JWT validation route
+router.get("/validate-token", validateJWT, (req, res) => {
+  res.status(200).send("Authenticated");
+});
+
+//POST auth routes
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
 router.post("/logout", authController.logout);
