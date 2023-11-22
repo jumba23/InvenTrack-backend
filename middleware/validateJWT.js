4085c9ddbacfdfe7e6021d5a82121355922342f6
Goran SD <g.cvetic23@gmail.com>
2023-11-22 06:43:24 -0800

    Add JWT validation middleware

--- middleware/validateJWT.js
+++ middleware/validateJWT.js
@@ -1,5 +1,15 @@
 const jwt = require("jsonwebtoken");
 
+//---------------------------------------------------------------------------------------------------------------
+// This middleware is used to validate JWTs sent with HTTP requests to secure endpoints on the server.
+// The middleware checks for a token in the request's cookies.
+// If a token is not present, it sends a 401 status response (Access Denied).
+// If a token is present, it attempts to verify the token using the secret key stored in process.env.JWT_SECRET.
+// If the token is valid, it attaches the verified user information to the request object and calls next() to pass
+// control to the next middleware or route handler.
+// If the token is invalid (verification fails), it sends a 400 status response (Invalid Token).
+// ---------------------------------------------------------------------------------------------
+
 // Middleware to validate JWT
 module.exports = function validateJWT(req, res, next) {
   const token = req.cookies.authToken;
