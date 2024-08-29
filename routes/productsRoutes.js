3a0e83961504e08c137d6a87123e24ae5ca6e1a0
Goran SD <g.cvetic23@gmail.com>
2024-08-29 12:21:33 -0700

    refactor: Update productsRoutes.js to use ES6 module syntax and improve comments

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -1,12 +1,75 @@
-const express = require("express");
-const router = express.Router();
-const productsController = require("../controllers/productsController");
-const validateJWT = require("../middleware/validateJWT");
+// productsRoutes.js
 
+import express from "express";
+import * as productsController from "../controllers/productsController.js";
+import validateJWT from "../middleware/validateJWT.js";
+
+/**
+ * Express router to mount product related functions on.
+ * @type {object}
+ * @const
+ * @namespace productsRoutes
+ */
+const router = express.Router();
+
+/**
+ * Route serving list of all products.
+ * @name get/
+ * @function
+ * @memberof module:routers/productsRoutes
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware (JWT validation)
+ * @param {callback} controller - Express controller function
+ */
 router.get("/", validateJWT, productsController.getAllProducts);
+
+/**
+ * Route serving product creation.
+ * @name post/
+ * @function
+ * @memberof module:routers/productsRoutes
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware (JWT validation)
+ * @param {callback} controller - Express controller function
+ */
 router.post("/", validateJWT, productsController.addProduct);
+
+/**
+ * Route serving single product by ID.
+ * @name get/:id
+ * @function
+ * @memberof module:routers/productsRoutes
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware (JWT validation)
+ * @param {callback} controller - Express controller function
+ */
 router.get("/:id", validateJWT, productsController.getProductById);
+
+/**
+ * Route serving product update.
+ * @name put/:id
+ * @function
+ * @memberof module:routers/productsRoutes
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware (JWT validation)
+ * @param {callback} controller - Express controller function
+ */
 router.put("/:id", validateJWT, productsController.updateProduct);
+
+/**
+ * Route serving product deletion.
+ * @name delete/:id
+ * @function
+ * @memberof module:routers/productsRoutes
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware (JWT validation)
+ * @param {callback} controller - Express controller function
+ */
 router.delete("/:id", validateJWT, productsController.deleteProduct);
 
-module.exports = router;
+export default router;
