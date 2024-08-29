2e0f4602e8e13866fee3d2482374375a245ef85c
Goran SD <g.cvetic23@gmail.com>
2024-08-29 11:34:52 -0700

    refactor: Update middleware/index.js to use ES6 module syntax and improve comments

--- middleware/index.js
+++ middleware/index.js
@@ -1,21 +1,22 @@
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const corsOptions = require("./corsOptions");
-const session = require("express-session");
-const cors = require("cors");
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import corsOptions from "./corsOptions.js";
+import session from "express-session";
+import cors from "cors";
 
-//The middleware is encapsulated in a module that exports a function accepting app and supabase as arguments. This design ensures that the middleware is easily attachable to your Express app and can interact with Supabase as needed.
-module.exports = (app, supabase) => {
+// The middleware is encapsulated in a module that exports a function accepting app and supabase as arguments.
+// This design ensures that the middleware is easily attachable to your Express app and can interact with Supabase as needed.
+const indexMiddleware = (app, supabase) => {
   // Security-related Middleware
   // -----------------------------
-  //essential for enabling requests from different origins, particularly useful when your frontend and backend are served from different domains.
+  // Essential for enabling requests from different origins, particularly useful when your frontend and backend are served from different domains.
   app.use(cors(corsOptions));
-  //parse the Cookie header and populate req.cookies with an object keyed by the cookie names.
+  // Parse the Cookie header and populate req.cookies with an object keyed by the cookie names.
   app.use(cookieParser());
 
   // Request Parsing Middleware
   // -----------------------------
-  //It uses body-parser to parse incoming request bodies in a middleware before your handlers, available under the req.body property.
+  // It uses body-parser to parse incoming request bodies in a middleware before your handlers, available under the req.body property.
   app.use(bodyParser.json());
 
   // Middleware to attach the Supabase client to requests
@@ -26,16 +27,17 @@ module.exports = (app, supabase) => {
 
   // Session Management Middleware
   // -----------------------------
-  //The middleware includes a function to attach the Supabase client to requests. This allows you to access Supabase functionalities easily within your request handling.
-  //This is crucial for maintaining user state and other data across multiple requests.
+  // This is crucial for maintaining user state and other data across multiple requests.
   app.use(
     session({
       secret: process.env.SESSION_SECRET, // Secret for signing the session ID cookie.
       resave: false,
       saveUninitialized: true,
       // In a production environment, it's best practice to use "true" to
-      //ensure cookies are transmitted over secure protocols only.
-      cookie: { secure: process.env.NODE_ENV === "production" }, //currently set to false for development
+      // ensure cookies are transmitted over secure protocols only.
+      cookie: { secure: process.env.NODE_ENV === "production" }, // Currently set to false for development
     })
   );
 };
+
+export default indexMiddleware;
