6a28288c7e935b1f69db083bb7297a03e8e0620a
Goran SD <g.cvetic23@gmail.com>
2024-08-29 12:21:57 -0700

    refactor: Update suppliersRoutes.js to use ES6 module syntax and improve comments

--- routes/suppliersRoutes.js
+++ routes/suppliersRoutes.js
@@ -1,12 +1,75 @@
-const express = require("express");
-const router = express.Router();
-const suppliersController = require("../controllers/suppliersController");
-const validateJWT = require("../middleware/validateJWT");
+// suppliersRoutes.js
 
+import express from "express";
+import * as suppliersController from "../controllers/suppliersController.js";
+import validateJWT from "../middleware/validateJWT.js";
+
+/**
+ * Express router to mount supplier related functions on.
+ * @type {object}
+ * @const
+ * @namespace suppliersRoutes
+ */
+const router = express.Router();
+
+/**
+ * Route serving list of all suppliers.
+ * @name get/
+ * @function
+ * @memberof module:routers/suppliersRoutes
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware (JWT validation)
+ * @param {callback} controller - Express controller function
+ */
 router.get("/", validateJWT, suppliersController.getAllSuppliers);
+
+/**
+ * Route serving supplier creation.
+ * @name post/
+ * @function
+ * @memberof module:routers/suppliersRoutes
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware (JWT validation)
+ * @param {callback} controller - Express controller function
+ */
 router.post("/", validateJWT, suppliersController.addSupplier);
+
+/**
+ * Route serving single supplier by ID.
+ * @name get/:id
+ * @function
+ * @memberof module:routers/suppliersRoutes
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware (JWT validation)
+ * @param {callback} controller - Express controller function
+ */
 router.get("/:id", validateJWT, suppliersController.getSupplierById);
+
+/**
+ * Route serving supplier update.
+ * @name put/:id
+ * @function
+ * @memberof module:routers/suppliersRoutes
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware (JWT validation)
+ * @param {callback} controller - Express controller function
+ */
 router.put("/:id", validateJWT, suppliersController.updateSupplier);
+
+/**
+ * Route serving supplier deletion.
+ * @name delete/:id
+ * @function
+ * @memberof module:routers/suppliersRoutes
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware (JWT validation)
+ * @param {callback} controller - Express controller function
+ */
 router.delete("/:id", validateJWT, suppliersController.deleteSupplier);
 
-module.exports = router;
+export default router;
