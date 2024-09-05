c06d6a1526442bff54a1dc966305346de1b2b1fd
Goran SD <g.cvetic23@gmail.com>
2024-09-04 22:30:01 -0700

    refactor: Improve validateRequest middleware to support different schema types

--- middleware/validateRequest.js
+++ middleware/validateRequest.js
@@ -1,7 +1,24 @@
 // middleware/validateRequest.js
-export const validateRequest = (schema) => {
+
+// function will  analyze the request object and validate it against the schema provided
+export const validateRequest = (schema, schemaType = "body") => {
   return (req, res, next) => {
-    const { error } = schema.validate(req.body, { abortEarly: false });
+    let dataToValidate;
+    switch (schemaType) {
+      case "body":
+        dataToValidate = req.body;
+        break;
+      case "query":
+        dataToValidate = req.query;
+        break;
+      case "params":
+        dataToValidate = req.params;
+        break;
+      default:
+        dataToValidate = req.body;
+    }
+
+    const { error } = schema.validate(dataToValidate, { abortEarly: false });
     if (error) {
       const errors = error.details.map((detail) => detail.message);
       return res.status(400).json({ errors });
