b57396456d9987dea6b9b10f6abcb3bc5de472d5
Goran SD <g.cvetic23@gmail.com>
2024-09-09 11:30:32 -0700

    refactor: Update login function to use secure cookie and set common top-level domain

--- controllers/authController.js
+++ controllers/authController.js
@@ -105,7 +105,7 @@ export const login = async (req, res) => {
       secure: process.env.NODE_ENV === "production",
       sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       domain:
-        process.env.NODE_ENV === "production" ? "vercel.app" : "localhost",
+        process.env.NODE_ENV === "production" ? ".vercel.app" : "localhost",
     });
     // Set cookie
     res.cookie("authToken", token, {
@@ -113,7 +113,7 @@ export const login = async (req, res) => {
       secure: process.env.NODE_ENV === "production", // Use secure in production - false in development
       sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // Use 'none' in production, 'lax' in development
       domain:
-        process.env.NODE_ENV === "production" ? "vercel.app" : "localhost", // Use vercel.app in production
+        process.env.NODE_ENV === "production" ? ".vercel.app" : "localhost", // Use vercel.app in production
       maxAge: 24 * 60 * 60 * 1000, // 24 hours
     });
     console.log("Cookie set, sending response");
